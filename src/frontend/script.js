3ad4afc2cc908ab95fe0f2cab03c38094652591c
José Pereira <jose.pereira@rnl.tecnico.ulisboa.pt>
2024-09-21 12:51:42 +0100

    fix: give each crypto its own toast style

--- src/frontend/script.js
+++ src/frontend/script.js
@@ -1,11 +1,11 @@
-var bgColors = [
-    "linear-gradient(to right, #F7931A, #FDE8D0)", // bitcoin
-    "linear-gradient(to right, #00b09b, #807CD8)", // solana
-    "linear-gradient(to right, #8C8C8C, #393939)", // ethereum
-    "linear-gradient(to right, #F26822, #4D4D4D)", // monero
-  ],
-  i = 0;
-
+const bgColors = [
+  "linear-gradient(to right, #F7931A, #FDE8D0)", // bitcoin
+  "linear-gradient(to right, #00b09b, #807CD8)", // solana
+  "linear-gradient(to right, #8C8C8C, #393939)", // ethereum
+  "linear-gradient(to right, #F26822, #4D4D4D)", // monero
+];
+const crypto = ["Bitcoin", "Solana", "Ethereum", "Monero"];
+let i = 0;
 document.getElementById("buy-bitcoin").addEventListener("click", function () {
   buy(1, "Bitcoin");
 });
@@ -23,6 +23,8 @@ document.getElementById("buy-monero").addEventListener("click", function () {
 });
 
 function buy(quantity, currency) {
+  // FIXME: later change to a custom backend
+  // e.g: cfetch(`/api/${crypto}-service?quantity=${quantity}`)
   const url = `http://localhost:8080/?quantity=${quantity}`;
   fetch(url)
     .then((response) => response.json())
@@ -34,12 +36,10 @@ function buy(quantity, currency) {
         duration: 3000,
         close: i % 3 ? true : false,
         style: {
-          background: bgColors[i % 4],
+          background:
+            bgColors[crypto.findIndex((element) => element === currency)],
         },
       }).showToast();
       i++;
-    }
-  );
+    });
 }
-
-
