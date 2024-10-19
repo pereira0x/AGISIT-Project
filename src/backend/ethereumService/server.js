864d8cffc3e37ac5ee1dcc912ca4b6f73d72a8c8
José Pereira <jose.pereira@rnl.tecnico.ulisboa.pt>
2024-10-19 14:30:58 +0100

    feat: change from google datastore to dockerized postgres

--- src/backend/ethereumService/server.js
+++ src/backend/ethereumService/server.js
@@ -1,30 +1,32 @@
-/* const { Datastore } = require("@google-cloud/datastore");
+const { Pool } = require("pg");
 
-const datastore = new Datastore();
+// Set up the connection pool
+const pool = new Pool({
+  user: "crypto",
+  host: "postgres", // Assuming you're running it locally with Docker
+  database: "crypto",
+  password: "password",
+  port: 5432,
+});
 
-function writeToDatastore(logMessage) {
-  const logEntity = {
-    key: datastore.key(["LogEntry"]),
-    data: {
-      timestamp: new Date(),
-      message: logMessage,
-    },
+// Function to write to Postgres database
+async function writeToPostgres(price) {
+  const logText = `A volume of Ethereum was requested. The price is ${price}$.`;
+
+  const query = {
+    text: "INSERT INTO transactions (log) VALUES ($1)",
+    values: [logText],
   };
 
-  datastore
-    .save(logEntity)
-    .then(() => {
-      console.log("Log entry saved to Datastore:", logMessage);
-    })
-    .catch((error) => {
-      console.error("Error writing log entry to Datastore:", error);
-    });
-} */
-
-function writeToDatastoreTemporary(logMessage) {
-  console.log("Log entry saved to Datastore:", logMessage);
+  try {
+    await pool.query(query);
+    console.log("Log entry saved to Postgres:", logText);
+  } catch (error) {
+    console.error("Error writing log entry to Postgres:", error);
+  }
 }
 
+// Express server setup
 var express = require("express");
 var app = express();
 
@@ -33,18 +35,19 @@ app.use(function (req, res, next) {
   next();
 });
 
-app.get("/", (req, res) => {
+app.get("/", async (req, res) => {
   const quantity = parseFloat(req.query["quantity"]);
 
   const currentEthereumPrice =
     Math.round(Math.random() * 1000.75 + 2000 * 100) / 100;
   const price = quantity * currentEthereumPrice;
-  writeToDatastoreTemporary(
-    `A ${req.query["quantity"]}x volume of Ethereum was requested. The price is ${price}$.`
-  );
+
+  // Log the transaction in Postgres
+  await writeToPostgres(price);
+
   res.send({ price });
 });
 
 app.listen(80, () => {
-  writeToDatastoreTemporary("ethereumService listening at port 80");
+  console.log("ethereumService listening at port 80");
 });
