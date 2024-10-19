d52e8b14f580bbca32c22c46cc3c6632978d01ff
José Pereira <jose.pereira@rnl.tecnico.ulisboa.pt>
2024-10-19 16:06:18 +0100

    fix: allow unfree (terraform)

--- flake.nix
+++ flake.nix
@@ -8,7 +8,10 @@
   outputs = { nixpkgs, flake-utils, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let
-        pkgs = import nixpkgs { inherit system; };
+        pkgs = import nixpkgs { inherit system; config = {
+              allowUnfree = true;
+            };
+          };
       in
       {
         devShell = pkgs.mkShell {
